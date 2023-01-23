fa164c956cc742da4443cd9edac6781d4f964774
hooloit <hooloit147@list.ru>
2023-01-23 18:31:21 +0500

    all changes

--- not main.py
+++ not main.py
@@ -6,7 +6,7 @@ from Game_Customization import MAP
 WIDTH = 1280
 HEIGHT = 720
 FPS = 60
-TILE_SIZE = 32
+TILE_SIZE = 64
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -27,49 +27,53 @@ def debug(info, y=10, x=10):
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
-        self.image = load_image("skeleton_idle.png")
+        self.image = load_image("bomb.png")
         self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
         self.obstacle_sprites = obstacle_sprites
 
     def update_Player(self):
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            self.direction.x -= 1
-        elif pygame.key.get_pressed()[pygame.K_RIGHT]:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
         else:
             self.direction.x = 0
-        if pygame.key.get_pressed()[pygame.K_UP]:
-            self.direction.y -= 1
-        elif pygame.key.get_pressed()[pygame.K_DOWN]:
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
             self.direction.y = 1
         else:
             self.direction.y = 0
 
     def move(self, speed):
-        self.rect.x += self.direction.x * speed
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.hitbox.x += self.direction.x * speed
         self.collision('horizontal')
-        self.rect.y += self.direction.y * speed
+        self.hitbox.y += self.direction.y * speed
         self.collision('vertical')
-
+        self.rect.center = self.hitbox.center
 
     def collision(self, direction):
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
-                if not sprite.self.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.x > 0:
-                        self.rect.right = sprite.rect.left
+                        self.hitbox.right = sprite.hitbox.left
                     if self.direction.x < 0:
-                        self.rect.left = sprite.rect.right
+                        self.hitbox.left = sprite.hitbox.right
         if direction == 'vertical':
             for sprite in self.obstacle_sprites:
-                if not sprite.self.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.y > 0:
-                        self.rect.bottom = sprite.rect.top
+                        self.hitbox.bottom = sprite.hitbox.top
                     if self.direction.y < 0:
-                        self.rect.top = sprite.rect.bottom
+                        self.hitbox.top = sprite.hitbox.bottom
 
     def update(self):
         self.update_Player()
@@ -79,8 +83,9 @@ class Player(pygame.sprite.Sprite):
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = load_image('Walk.png')
+        self.image = load_image('boom.png')
         self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
 
 
 def load_image(name, colorkey=None):
@@ -123,22 +128,24 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        debug(self.player.direction)
+
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
-        self.half_heigth = self.display_surface.get_size()[1] // 2
-
+        self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
     def custom_draw(self, player):
         self.offset.x = player.rect.centerx - self.half_width
-        self.offset.y = player.rect.centery - self.half_heigth
+        self.offset.y = player.rect.centery - self.half_height
         for sprite in self.sprites():
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
+
 
 class Game:
     def __init__(self):
@@ -158,6 +165,7 @@ class Game:
             self.Level.run()
             pygame.display.update()
             self.clock.tick(FPS)
+
 
 if __name__ == '__main__':
     game = Game()
